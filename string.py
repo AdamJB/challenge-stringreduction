4395853b698879946ff4ce7e77da7e8297cd7a9a
Adam Bednarek <adamjb51@gmail.com>
2012-09-12 10:11:25 -0400

    Clean code

--- string.py
+++ string.py
@@ -9,41 +9,6 @@ def getData():
 
   return llStrings
 
-def getNumResults(string):
-  # Doesn't work .. :(
-  # If  even # of each letter => 2 (ex:bac, bbaacc, cababc)
-  # If uneven # of each letter => 1 (ex: bbaccc)
-  # If all same letters => same # letter (cccccc)
-  # Even # of leters with 2 of 2 char: ex: bbaa => bca => aa)
-  numA = 0
-  numB = 0
-  numC = 0
-  for s in string:
-    if s == "a":
-      numA += 1
-    if s == "b":
-      numB += 1
-    if s == "c":
-      numC += 1
-
-
-  # If there's only 1 letter, return length of string
-  if numA == len(string) or numB == len(string) or numC == len(string):
-    return len(string)
-
-  # If there are even number of letters for all 3 chars:
-  if numA == numB and numB == numC:
-    return 2
-
-  if numA == numB and numC == 0 and (numA % 2 == 0):
-    return 2
-  if numA == numC and numB == 0 and (numA % 2 == 0):
-    return 2
-  if numB == numC and numA == 0 and (numB % 2 == 0):
-    return 2
-
-  return 1
-
 def reduceString(string):
   # Create 3 stacks containing each character
   # We'll be pushing and poping to each stack.
@@ -116,5 +81,4 @@ def reduceString(string):
 strings = getData()
 
 for s in strings:
-  #print getNumResults(s)
   print reduceString(s)
